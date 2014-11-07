777c84770a67d6f47ac3ab65d13b0c447f27439b
David Ha <dha@1493.net>
2014-11-07 08:49:15 -0500

    Add save sermon series route

--- src/main/resources/app/app-controllers.js
+++ src/main/resources/app/app-controllers.js
@@ -5,6 +5,10 @@ angular.module('contentControllers', ['ngRoute', 'contentServices'])
 							controller: 'SermonSeriesListCtrl',
 							templateUrl: '/app/sermon-series/list.html'
 						})
+						.when('/sermon-series-add', {
+							controller: 'SermonSeriesAddCtrl',
+							templateUrl: '/app/sermon-series/add.html'
+						})
 						.when('/sermon-series/:id', {
 							controller: 'SermonSeriesEditCtrl',
 							templateUrl: '/app/sermon-series/edit.html'
@@ -14,13 +18,21 @@ angular.module('contentControllers', ['ngRoute', 'contentServices'])
 		.controller('SermonSeriesListCtrl', ['$scope', 'contentApi', '$log', function($scope, contentApi, $log) {
 			$scope.sermonSeriesList = contentApi.all('sermon-series').getList().$object;
 		}])
-		.controller('SermonSeriesAddCtrl', ['$scope', 'contentApi', '$log', function($scope, contentApi, $log) {
-			$scope.save = function() {
+		.controller('SermonSeriesAddCtrl', ['$scope', 'contentApi', '$location', '$log', function($scope, contentApi, $location, $log) {
+			$scope.onSave = function() {
 				var transientSermonSeries = { name: $scope.name, description: $scope.description };
 				
-				$log.info("saving " + transientSermonSeries);
+				$log.info('adding sermon series ' + JSON.stringify(transientSermonSeries));
 				
-				contentApi.post('sermon-series', transientSermonSeries);
+				contentApi.all('sermon-series').customPOST(transientSermonSeries).then(
+						function(persistentSermonSeries) {
+							$log.info('added sermon series ' + persistentSermonSeries.id);
+							
+							$location.path('/');
+						},
+						function(reason) {
+							$log.error('could not add sermon series: ' + reason);
+						});
 			};
 		}])
 		.controller('SermonSeriesEditCtrl', ['$scope', '$routeParams', '$log', 'contentApi', function($scope, $routeParams, $log, contentApi) {
